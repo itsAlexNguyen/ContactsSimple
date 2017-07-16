74f306e56b191aed105e050b05f0ef6601f76d92
Alex Nguyen <alex.nguyen@td.com>
2017-07-16 00:37:52 -0400

    Worked on action bar and animations

--- app/src/main/java/com/itsalexnguyen/contactssimple/binders/ContactActionBinder.java
+++ app/src/main/java/com/itsalexnguyen/contactssimple/binders/ContactActionBinder.java
@@ -1,7 +1,10 @@
 package com.itsalexnguyen.contactssimple.binders;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.itsalexnguyen.contactssimple.viewholders.ContactActionViewHolder;
 
@@ -14,6 +17,13 @@ public class ContactActionBinder extends AbstractDataBinder<ContactActionViewHol
 
     @Override
     public void bindViewHolder(ContactActionViewHolder holder) {
-        // TODO
+        Context ctx = holder.callButton.getContext();
+
+        Animation slideLeft = AnimationUtils.loadAnimation(ctx, android.R.anim.slide_in_left);
+
+        holder.callButton.startAnimation(slideLeft);
+        holder.messageButton.startAnimation(slideLeft);
+        holder.videoButton.startAnimation(slideLeft);
+        holder.emailButton.startAnimation(slideLeft);
     }
 }
